20cf254dded0864b8ce6bbe04b8a359059fd6331
Saifalikhan Pathan <78328030+Saifak34@users.noreply.github.com>
2024-06-12 23:33:42 -0400

    2nd change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          dhan te nan aaja ss dil nichode
+          dhan te nan aaja sssss dil nichode
         </p>
         <a
           className="App-link"
